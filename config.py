0cd87076dd2eef63264b675d9ec14b3ef689bbc6
Jean Penso <95495939+Jean2208@users.noreply.github.com>
2023-04-11 01:02:04 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-DISCORD_API_TOKEN ='Your Discord API token here' # PLACE YOUR TOKEN
+DISCORD_BOT_TOKEN ='Your Discord Bot token here' 
 EXCEL_SHEET_PATH = r'C:\\Users\\file\\path\\here\\filename.xlsx' # DEFINE THE PATH WHERE YOUR SHEET IS LOCATED
 SERVER_NAME = "juan pablo's server" 
 ROLE_NAME = 'internsummer' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
