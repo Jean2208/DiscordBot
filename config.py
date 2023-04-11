929d187a3734a9477dde26d01d4d97f4db59cec3
Jean Penso <95495939+Jean2208@users.noreply.github.com>
2023-04-11 12:28:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DISCORD_BOT_TOKEN ='Your Discord Bot token here'
 EXCEL_SHEET_PATH = r'C:\\Users\\file\\path\\here\\filename.xlsx' # DEFINE THE PATH WHERE YOUR SHEET IS LOCATED
 SERVER_NAME = 'Cysec Resource Hub (Career, Networking & Continuous Learning)' 
 ROLE_NAME = 'intern_summer2023' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
-ROW_NAME = 'Your name' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
+ROW_NAME = 'Name of the row where all the names are. Ex: Names, Your name, Full names' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
 
 
 
