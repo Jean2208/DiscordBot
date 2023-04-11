1f8699e06743a0b39b0e8437408471f90960f9ba
Jean Penso <95495939+Jean2208@users.noreply.github.com>
2023-04-11 12:39:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 DISCORD_BOT_TOKEN ='Your Discord Bot token here' 
 EXCEL_SHEET_PATH = r'C:\\Users\\file\\path\\here\\filename.xlsx' # DEFINE THE PATH WHERE YOUR SHEET IS LOCATED
 SERVER_NAME = 'Cysec Resource Hub (Career, Networking & Continuous Learning)' 
