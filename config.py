57c1606b9f6e67461b014db42cd6d8fd43069db1
Jean Penso <jeanpaulpenso01@live.com>
2023-04-11 00:57:06 -0500

    commit

---
+++ config.py
@@ -0,0 +1,10 @@
+
+DISCORD_API_TOKEN ='Your Discord API token here' # PLACE YOUR TOKEN
+EXCEL_SHEET_PATH = r'C:\\Users\\file\\path\\here\\filename.xlsx' # DEFINE THE PATH WHERE YOUR SHEET IS LOCATED
+SERVER_NAME = "juan pablo's server" 
+ROLE_NAME = 'internsummer' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
+ROW_NAME = 'Your name' # YOU MAY HAVE TO CHANGE THIS ACCORDINGLY
+
+
+
+
--- roles.py
+++ roles.py
@@ -1,5 +1,6 @@
 import discord
 import pandas as pd
+from config import *
 
 intents = discord.Intents.default()
 intents.members = True
@@ -12,23 +13,23 @@ async def on_ready():
     except Exception as e:
         print(f"Error in on_ready event handler: {e}")
         
-    df = pd.read_excel(r'C:\\Users\\jeanp\Desktop\\names.xlsx')
+    df = pd.read_excel(EXCEL_SHEET_PATH) 
 
     # Get a list of all the members in the server
-    guild = discord.utils.get(client.guilds, name="juan pablo's server")
+    guild = discord.utils.get(client.guilds, name=SERVER_NAME)
     members = []
     async for member in guild.fetch_members(limit=None):
         members.append(member)
 
     # Loop through each name in the spreadsheet
-    for row in df.iterrows():
-        name = row['Name']
+    for _, row in df.iterrows():
+        name = row.loc[ROW_NAME]
         for member in members:
             # Lower case the name and look if there's a match between discord and the excel spreadsheet
             if name.lower() == member.display_name.lower():
                 # Change the role of the member
-                role = discord.utils.get(guild.roles, name='Special Role')
+                role = discord.utils.get(guild.roles, name=ROLE_NAME)
                 await member.add_roles(role)
-                print(f"{member.display_name} has been given the Special Role.")
+                print(f"{member.display_name} has been given the {ROLE_NAME} role.")
 
-client.run('TOKEN')
+client.run(DISCORD_API_TOKEN)
